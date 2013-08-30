3c295f0de2b1c067a7824e66bb4dbf7c8eb917f0
Rezelk <zephyr.tt@gmail.com>
2013-08-30 17:53:58 +0900

    初期資産登録

---
+++ poker-core.js
@@ -0,0 +1,45 @@
+/**
+ * ポーカー
+ * @namespace
+ */
+var poker = {};
+
+/**
+ * ポーカー・ハンド
+ * @enum {number}
+ */
+poker.handCategory = {
+  HIGH_CARD       :   0,
+  ONE_PAIR        : 100,
+  TWO_PAIR        : 200,
+  THREE_OF_A_KIND : 300,
+  STRAIGHT        : 400,
+  FLUSH           : 500,
+  FULL_HOUSE      : 600,
+  FOUR_OF_A_KIND  : 700,
+  STRAIGHT_FLUSH  : 800,
+  ROYAL_FLUSH     : 900
+};
+
+
+/**
+ * コアライブラリ
+ * @namespace
+ */
+poker.core = {};
+
+
+/**
+ * 役判定する。
+ *
+ * @param {{rank:number, suit:string}[]} cards 手札。
+ * @returns poker.handCategory のどれか。
+ */
+poker.core.getHandCategory = function(cards) {
+  // TODO: ここに処理を実装します。
+
+
+
+  // どの組み合わせにも当てはまらないならば、ハイカードを返す。
+  return poker.handCategory.HIGH_CARD;
+}
